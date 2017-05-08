fa0e3af44819dad4f979cc7c596644d6a1f11509
deonslowcole <dcole@fullsail.edu>
2017-05-07 22:39:09 -0400

    Initial Commit

---
+++ DCole_iOS_UsersAndData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DCole_iOS_UsersAndData
+//
+//  Created by Deon Cole on 5/7/17.
+//  Copyright © 2017 Deon Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
